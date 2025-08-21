70b159098e8a3b70cceacf0c5d35c7858c25b630
romro <romainbhr21@gmail.com>
2025-08-22 00:42:37 +0200

    - Mise en place des potentiels fonts - Commencement de l'accueil et du css

--- API/vues/pages/v_accueil.inc.php
+++ API/vues/pages/v_accueil.inc.php
@@ -1,1 +1,8 @@
-<h1>Hello world !</h1>
+<section>
+    <div class="photo_de_profil">
+        <img width="100" height="100" src="<?= VariablesConfig::$url . "images/" ?>" alt="Moi">
+    </div>
+    <h1 class="dev-badge dev-gradient Press_Start2P">Boehler Romain</h1>
+    <p>Full-Stack Développeur & Créative Technologie</p>
+    <p>Je crée des expériences numériques qui allient technologie de pointe et design époustouflant. Des applications web à l'automatisation industrielle, je donne vie à vos idées.</p>
+</section>
